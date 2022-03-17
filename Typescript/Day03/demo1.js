48010f39ccac4fe82d48b05a543dba11b1858bba
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-03-17 11:54:51 +0530

    Day03

---
+++ Typescript/Day03/demo1.js
@@ -0,0 +1,20 @@
+//System.out.println()
+// console.log("Hello World");
+var a1;
+var a2, a3;
+var a_3, a$2;
+// data Type
+// number(int,double,float,short,long),string(''/""),boolean(true/false),
+//any(number,string,boolean) ,void (netural data typw)
+// misce. null & undefined 
+var b = 10; // Forward declaration & defonition
+// console.log('Value of B is '+b);
+var b1;
+// console.log("Value of b1 is "+b1)
+var b2;
+b2 = 2.5;
+console.log("Value of b2 is ".concat(b2, " "));
+b2 = 'Sumit';
+console.log("Value of b2 is ".concat(b2, " "));
+b2 = true;
+console.log("Value of b2 is ".concat(b2, " "));
