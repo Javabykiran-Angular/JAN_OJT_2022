28210494e00d211dd7178b9b193771be8adb677e
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-03-21 11:38:17 +0530

    Day04

---
+++ Typescript/Day04/demo1.js
@@ -0,0 +1,61 @@
+//Type Assertion
+var a;
+//1 Angle Bracket
+//2 as syntax
+//1 Angle bracket 
+var temp = a;
+// temp.
+//2 as syntax
+var temp2 = a;
+// temp2.
+// Operators
+// Airthematic => +,-,*,/,%
+// Logical => &&,||
+// Conditional => !=,>=,<=,==(its only checks value),===(Strongly equality it checks its data type as well as its value e.g a===b)
+// Bitwise=> &,|,^,!,~,<<,>>
+// Relational => <,>,==
+// Assignment => =.+=,-=,/=,*=
+// ternary => condition ? Expression1 : expression2
+// unary => post/pre dec/inc ++,--,++a,a--
+// Control statement
+// if,if-else,nested if-else,break,continue,switch
+// if(2<4){
+//   console.log("Condition is true");
+// }
+var a1 = 21;
+// if(a1<4){
+//   console.log("Condition is true");
+// }else{
+//   console.log("Condition is False");
+// }
+var choice = 2;
+// switch(choice){
+//   case 1 : console.log("U r in case 1");
+//           break;
+//   case 2 : console.log("U r in case 2");
+//           break;
+//   default:console.log("U r in default case");
+//            break;
+// }
+// Loop Statement
+//for,while,do-while,foreach
+// while
+var count = 5;
+// 
+// while(count!=0){
+//   console.log("Count is "+count)
+//   count--;
+// }
+//do-while
+// do{
+//   console.log("---- Count--- "+count);
+//   count--;
+// }while(count!=0)
+// for(int a=0;a<4;a++){}
+// for(var a2=0;a2<4;a2++){
+//   console.log("A2 value is "+a2)
+// }
+// console.log("After for loop "+a2)
+for (var i = 0; i < 4; i++) {
+    console.log("i value is " + i);
+}
