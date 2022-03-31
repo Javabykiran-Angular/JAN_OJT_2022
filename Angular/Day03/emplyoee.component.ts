cdfc2f6aed7fce5a5b092c4345bfdc4e76052dcf
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-03-31 11:47:25 +0530

    Day03

---
+++ Angular/Day03/emplyoee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-employee',
+  templateUrl:'./emplyoee.component.html',
+  styleUrls:['./emplyoee.component.css']
+})
+export class EmployeeComponent{
+  
+}
