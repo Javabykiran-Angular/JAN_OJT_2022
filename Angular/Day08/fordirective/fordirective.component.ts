6f8bb06a0fb483c649d370ecd57c72af25d9a107
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-04-07 11:52:58 +0530

    Day08

---
+++ Angular/Day08/fordirective/fordirective.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fordirective',
+  templateUrl: './fordirective.component.html',
+  styleUrls: ['./fordirective.component.css']
+})
+export class FordirectiveComponent implements OnInit {
+
+  arrProduct=[
+    {
+      productName:'Samsung',
+      price:25000,
+      quantity:1
+    },
+    {
+      productName:'Motorola',
+      price:35000,
+      quantity:1
+    },
+    {
+      productName:'Realme',
+      price:15000,
+      quantity:1
+    },
+    {
+      productName:'OnePlus',
+      price:45000,
+      quantity:1
+    }
+  ];
+  isreadonly:boolean=false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onKeyDown(character){
+    let temp:string=character.value;
+    if(temp.length<=5){
+
+    }else{
+      character.style.background='red';
+      character.style.color='white'
+      this.isreadonly=true;
+      alert("U over the limit of character")
+    }
+
+  }
+
+}
