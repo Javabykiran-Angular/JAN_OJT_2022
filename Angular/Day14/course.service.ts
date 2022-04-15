f62306d91565968ab50d8b5c793c6679a1aafc78
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-04-15 12:39:06 +0530

    Day14

---
+++ Angular/Day14/course.service.ts
@@ -0,0 +1,10 @@
+
+export class CourseService{
+
+    private course:string[]=['Angular 8','React Js','Vue Js','Embber Js'];
+
+    getCourse(){
+      return (this.course);
+    }
+
+}
---
+++ Angular/Day14/course/course.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../course.service';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  myCourse:string[]=[];
+
+  constructor(private service:CourseService) { 
+
+    //let service=new CourseService();
+
+  }
+
+  ngOnInit()
+  {
+    this.myCourse=this.service.getCourse();
+  }
+  
+
+}
