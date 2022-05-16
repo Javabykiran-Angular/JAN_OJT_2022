964b38eb867d25d76cb0a9a45efa3e0eccae4fc2
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2022-05-16 13:18:32 +0530

    Day30

---
+++ Angular/Day30/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ParticlesModule } from 'angular-particle'; 
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HeaderComponent } from './header/header.component';
+import { EmpdetailsComponent } from './empdetails/empdetails.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';  
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material.module';
+import { MatConfirmDialogComponent } from './shared/mat-confirm-dialog/mat-confirm-dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NotfoundComponent,
+    HeaderComponent,
+    EmpdetailsComponent,
+    AddemployeeComponent,
+    UpdateEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ParticlesModule,
+    HttpClientModule,
+    ModalModule.forRoot(),
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents:[MatConfirmDialogComponent]
+})
+export class AppModule { }
